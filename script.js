80b479d05bbc6f3b9f0369e56a7033119afccb83
nauvaldis <sigantengnoval@gmail.com>
2021-09-29 11:29:28 +0700

    Timer logic revision

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ window.addEventListener('beforeunload', function (e) {
     localStorage.setItem('closed-time', new Date());
     localStorage.setItem('Time', JSON.stringify({hours, minutes, seconds}));
     localStorage.setItem('stat', status);
-
 });
 
 window.addEventListener('load', function (e) {
@@ -24,14 +23,31 @@ window.addEventListener('load', function (e) {
     let duration = JSON.parse(localStorage.getItem('Time'));
     let time_to_sec = calculateDuration(duration.hours, duration.minutes, duration.seconds);
 
+    // timer still running when the tab closed or refreshed
     if (localStorage.getItem('stat') === "paused") {
         seconds = Math.floor(elapsed_time + time_to_sec);
-        startTimer();
-        console.log(seconds);
+
+        // get multiple return value
+        // from https://stackoverflow.com/questions/2917175/return-multiple-values-in-javascript
+        let valueTime = secondToTime(seconds)
+        hours = valueTime[0]
+        minutes = valueTime[1]
+        seconds = valueTime[2] - 1 // -1 because startTimer() starting with seconds++
+        startTimer() // set time when first load
+        startstopcontinue(); // rev for timer auto play when refresh
+    }
+    
+    // timer paused when the tab closed or refreshed
+    if (localStorage.getItem('stat') === "continue") {
+        hours = duration.hours
+        minutes = duration.minutes
+        seconds = duration.seconds - 1 // -1 because startTimer() starting with seconds++
+        status = "paused"
+        startTimer()
+        startstopcontinue()
     }
 
 });
-
 
 function startTimer(){
     seconds++;
@@ -106,4 +122,12 @@ function calculateDuration(hour, minute, second) {
     return duration;
 }
 
-
+// convert from second to hours:minutes:second
+// from https://stackoverflow.com/questions/6312993/javascript-seconds-to-time-string-with-format-hhmmss
+function secondToTime(second) {
+    var sec_num = parseInt(second, 10);
+    var sec2hours   = Math.floor(sec_num / 3600);
+    var sec2minutes = Math.floor((sec_num - (sec2hours * 3600)) / 60);
+    var sec2seconds = sec_num - (sec2hours * 3600) - (sec2minutes * 60);
+    return [sec2hours, sec2minutes, sec2seconds];
+}
